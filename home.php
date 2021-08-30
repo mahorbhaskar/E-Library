84a2fabbc4c8da05ef97bde37646d380ff532f01
Bhaskar Mahor <51980844+mahorbhaskar@users.noreply.github.com>
2021-08-30 20:19:53 +0530

    Update home.php

--- home.php
+++ home.php
@@ -263,7 +263,7 @@ if (!isset($_SESSION['username']))
       integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
       crossorigin="anonymous"
     ></script>
-    <script src="script/tilt.js"></script>
+<!--     <script src="script/tilt.js"></script> -->
 
 </body>
 </html>
